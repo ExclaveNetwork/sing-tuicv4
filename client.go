ffa2c4c294865ce0a41bd2dc2baf6f93555b2ff3
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2026-08-03 14:58:31 +0800

    Fix UDP sessions not closed on connection close
    cherry-picked from https://github.com/SagerNet/sing-quic/commit/d83826c306d7c008cafb3fd6d8ee07cbcfd656ed

--- client.go
+++ client.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"io"
 	"net"
+	"os"
 	"runtime"
 	"sync"
 	"time"
@@ -259,6 +260,12 @@ func (c *Client) ListenPacket(ctx context.Context) (net.PacketConn, error) {
 		conn.udpAccess.Unlock()
 	})
 	conn.udpAccess.Lock()
+	select {
+	case <-conn.connDone:
+		conn.udpAccess.Unlock()
+		return nil, E.Errors(conn.connErr, os.ErrClosed)
+	default:
+	}
 	sessionID = conn.udpSessionID
 	conn.udpSessionID++
 	conn.udpConnMap[sessionID] = clientPacketConn
@@ -324,7 +331,14 @@ func (c *clientQUICConnection) active() bool {
 func (c *clientQUICConnection) closeWithError(err error) {
 	c.closeOnce.Do(func() {
 		c.connErr = err
+		c.udpAccess.Lock()
 		close(c.connDone)
+		udpConnMap := c.udpConnMap
+		c.udpConnMap = make(map[uint32]*udpPacketConn)
+		c.udpAccess.Unlock()
+		for _, udpConn := range udpConnMap {
+			udpConn.closeWithError(err)
+		}
 		_ = c.quicConn.CloseWithError(0, "")
 		_ = c.rawConn.Close()
 	})
